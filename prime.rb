6441f7eaf038cd99e4c1b423a584ed243227c9f8
Amanda Walker <walker.akw@gmail.com>
2020-07-10 03:38:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ def prime?(int)
   return false if int <=1
   return true if int ==2
   #(2...int-1).each {|factor| return false if int % factor == 0}
-  return false if ((2...int-1).each.any {|factor| int % factor == 0})
+  return false if ((2...int-1).each.any? {|factor| int % factor == 0})
   return true
 end
